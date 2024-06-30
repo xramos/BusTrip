1635e0dfd6be1533817c3b2e882ff8d43d5744a6
Xavi <xavier.ramos@wefox.com>
2024-06-30 21:10:33 +0200

    Adding StopInfoView

---
+++ BusTrip/BusTripTests/Views/StopInfoViewSnapshotTests.swift
@@ -0,0 +1,21 @@
+//
+//  StopInfoViewSnapshotTests.swift
+//  BusTripTests
+//
+//  Created by Xavier Ramos on 30/6/24.
+//
+
+import XCTest
+import PreviewSnapshotsTesting
+
+@testable import BusTrip
+
+final class StopInfoViewSnapshotTests: XCTestCase {
+
+    func test_snapshots() {
+        
+        // isRecording = true
+        StopInfoView_Previews.snapshots.assertSnapshots(as: .image(perceptualPrecision: 0.98,
+                                                                   layout: .device(config: .iPhoneX(.portrait))))
+    }
+}
